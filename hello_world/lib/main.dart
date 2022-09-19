a439e2f42cfc19b4384babd4a26cfa33a24603ec
Erica Rodrigues <ericamrodrigs@gmail.com>
2022-09-19 19:02:47 -0300

    hello world

---
+++ hello_world/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:hello_world/my_app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
---
+++ hello_world/lib/my_app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:hello_world/home_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Hello World'),
+    );
+  }
+}
