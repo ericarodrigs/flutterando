32f1132c1cb773dce96a74cdc9b01df94975f3eb
Erica Rodrigues <ericamrodrigs@gmail.com>
2022-10-09 18:54:40 -0300

    Wraping the form with Card widget

--- textformfiel/lib/login_page.dart
+++ textformfiel/lib/login_page.dart
@@ -14,69 +14,78 @@ class _LoginPage extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Stack(
-      children: [
-        SizedBox(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          child: Image.asset(
-            'assets/images/background.jpeg',
-            fit: BoxFit.cover,
-          ),
-        ),
-        SingleChildScrollView(
-          child: SizedBox(
+      body: Stack(
+        children: [
+          SizedBox(
             width: MediaQuery.of(context).size.width,
             height: MediaQuery.of(context).size.height,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    height: 20,
-                  ),
-                  TextField(
-                    onChanged: (text) {
-                      email = text;
-                    },
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: const InputDecoration(
-                        labelText: 'Email', border: OutlineInputBorder()),
-                  ),
-                  const SizedBox(
-                    height: 8,
-                  ),
-                  TextField(
-                    onChanged: (text) {
-                      password = text;
-                    },
-                    obscureText: true,
-                    decoration: const InputDecoration(
-                        labelText: 'Password', border: OutlineInputBorder()),
-                  ),
-                  const SizedBox(height: 16),
-                  ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        primary: Colors.blueGrey,
-                        elevation: 0,
-                        minimumSize: const Size(double.infinity, 30),
-                        textStyle: const TextStyle(fontSize: 18),
+            child: Image.asset(
+              'assets/images/background.jpeg',
+              fit: BoxFit.cover,
+            ),
+          ),
+          SingleChildScrollView(
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Card(
+                      child: Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: Column(
+                          children: [
+                            TextField(
+                              onChanged: (text) {
+                                email = text;
+                              },
+                              keyboardType: TextInputType.emailAddress,
+                              decoration: const InputDecoration(
+                                  labelText: 'Email', border: OutlineInputBorder()),
+                            ),
+                            const SizedBox(
+                              height: 8,
+                            ),
+                            TextField(
+                              onChanged: (text) {
+                                password = text;
+                              },
+                              obscureText: true,
+                              decoration: const InputDecoration(
+                                  labelText: 'Password', border: OutlineInputBorder()),
+                            ),
+                            const SizedBox(height: 16),
+                            ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  primary: Colors.blueGrey,
+                                  onPrimary: Colors.white,
+                                  elevation: 0,
+                                  minimumSize: const Size(double.infinity, 30),
+                                  textStyle: const TextStyle(fontSize: 18),
+                                ),
+                                onPressed: () {
+                                  if (email == 'erica@erica.com' &&
+                                      password == '1234') {
+                                    Navigator.of(context).pushReplacementNamed('/home');
+                                  } else {
+                                    print('Login inválido');
+                                  }
+                                },
+                                child: const Text('Entrar')),
+                          ],
+                        ),
                       ),
-                      onPressed: () {
-                        if (email == 'erica@erica.com' && password == '1234') {
-                          Navigator.of(context).pushReplacementNamed('/home');
-                        } else {
-                          print('Login inválido');
-                        }
-                      },
-                      child: const Text('Entrar')),
-                ],
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
-        ),
-      ],
-    ));
+        ],
+      ),
+    );
   }
 }
